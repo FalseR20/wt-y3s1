80306267a13766d73162ae133bf4280e1fce5376
FalseR20 <kno3.2002@gmail.com>
2022-12-05 22:33:52 +0300

    feat(lab1): save

--- lab1/__main__.py
+++ lab1/__main__.py
@@ -1,13 +1,16 @@
-from flask import Flask
+#!/usr/bin/env python
+
+from flask import Flask, Blueprint
 from flask_restful import Api
 
-import resources
+from resources import example
 
 app = Flask(__name__)
 api = Api(app)
-api.add_resource(resources.ArgsResource, "/api/data_in_args")
-api.add_resource(resources.ParamsResource, "/api/data_in_params/<name>/<place>")
-api.add_resource(resources.FormResource, "/api/data_in_form")
+app.register_blueprint(api_blueprint := Blueprint("/api", __name__))
+api.add_resource(example.ArgsResource, "/api/data_in_args")
+api.add_resource(example.ParamsResource, "/api/data_in_params/<name>/<place>")
+api.add_resource(example.FormResource, "/api/data_in_form")
 
 if __name__ == "__main__":
     app.run(debug=False)
